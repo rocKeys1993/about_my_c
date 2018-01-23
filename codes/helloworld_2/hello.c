9224e5c8369746b296b8958cd61df1339703ef5b
李鹏阳 <liaoya1993@foxmail.com>
2018-01-23 14:30:30 +0800

    move file path,and rename filename

---
+++ codes/helloworld_2/hello.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+/* hello world test programmer */
+
+/* .......
+
+ * 1....
+ * 2....
+ * 3....
+ * 4....
+
+ */
+
+// annotation test
+
+int main()
+{
+    printf("hello boy,and your dream \n");
+    return 0;
+}
